a4e3aefa0f5080c833db9c342a26c55921f898e2
Al-Amin <alaminkhanshakil@gmail.com>
2025-02-03 13:54:40 +0600

    Write CRUD method for category controllers

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,8 @@ class Category < ApplicationRecord
   extent FriendlyID
   friendly_id :name, use: :slugged
 
-  has_many :subcategories, class_name: "Category", foreign_key: "parent_id", dependent: :destroy
-  belongs_to :parent_category, class_name: "Category", optional: true
+  has_many :subcategories, class_name: 'Category', foreign_key: 'parent_id', dependent: :destroy
+  belongs_to :parent_category, class_name: 'Category', optional: true
 
   validates :name, presence: true
   validates :name, uniqueness: { scope: :parent_id }
