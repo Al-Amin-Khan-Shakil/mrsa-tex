b8abf6afc70b8475c5fffe036afd4572e06496d2
Al-Amin <alaminkhanshakil@gmail.com>
2025-02-05 18:10:11 +0600

    Product index view

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ApplicationRecord
 
   has_many_attached :images
 
-  accepts_nested_attributes_for :counties, allow_destroy: true
+  accepts_nested_attributes_for :variant_names, allow_destroy: true
 
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
