b3010326d0705f616f5b62dede4e71ad6fdf6957
Al-Amin <alaminkhanshakil@gmail.com>
2025-02-05 14:43:35 +0600

    Create product controller for create and read methods only

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,39 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.includes(variant_names: :variant_values).find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+    @product.variant_names.build.variant_values.build
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :category_id,
+      :description,
+      :price,
+      :stock,
+      images: [],
+      variant_names_attributes: [:id, :name, :product_id, :_destroy,
+                                 { variant_values_attributes: %i[id value price stock variant_name_id image _destroy] }]
+    )
+  end
+end
