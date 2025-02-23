eba11f7d4d0e4d7641fd4d66b5a0eb8308d1bd9c
Al-Amin <alaminkhanshakil@gmail.com>
2025-02-23 12:25:39 +0600

    Update the district and subdistrict model

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,2 +1,6 @@
 class District < ApplicationRecord
+  has_many :sub_districts, dependent: :destroy
+  accepts_nested_attributes_for :sub_districts, allow_destroy: true
+
+  validates :name, presence: true
 end
--- app/models/sub_district.rb
+++ app/models/sub_district.rb
@@ -1,3 +1,5 @@
 class SubDistrict < ApplicationRecord
   belongs_to :District
+
+  validates :name, presence: true
 end
