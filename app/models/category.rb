a75310ccc49dc1af81dd044e577971f88521a6ad
Al-Amin-Khan-Shakil <alaminkhanshakil@gmail.com>
2025-03-15 04:19:38 +0600

    Fix category update issue

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,11 +8,22 @@ class Category < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: { scope: :parent_id }
 
+  after_update :update_subcategory_slug, if: :saved_change_to_name?
+
   def slug_candidates
     parent_category ? ["#{parent_category.name}-#{name}"] : [name]
   end
 
   def should_generate_new_friendly_id?
     name_changed? || super
+  end
+
+  private
+
+  def update_subcategory_slug
+    subcategories.find_each do |subcategory|
+      subcategory.slug = nil
+      subcategory.save!
+    end
   end
 end
